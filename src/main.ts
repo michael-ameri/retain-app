c041e101e4a3ee5dacf19e18c52da0d46bdad98f
Michael <michael.ameri@gmail.com>
2016-12-03 13:57:31 +0100

    4 - Input/Event: [ui-field]=input, (event)=calledMethod(), {{display.value}}

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,16 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser'
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
 import {App} from './app'
-import {Main} from './app/containers'
-import {AppBar} from './app/ui'
+import {Main, Notes} from './app/containers'
+import {AppBar, NoteCard} from './app/ui'
 
 @NgModule({
     declarations: [
         App,
         Main,
+        Notes,
         AppBar,
+        NoteCard,
     ],
     imports: [BrowserModule],
     bootstrap: [App]
