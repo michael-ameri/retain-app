64ef71901eca1db40ae79e14ceb8e5969455e41e
Michael <michael.ameri@gmail.com>
2016-12-03 15:46:43 +0100

    8 - create note v2: add it to display with event

--- src/app/ui/note-creator.ts
+++ src/app/ui/note-creator.ts
@@ -1,4 +1,8 @@
-import {Component} from '@angular/core';
+import {
+    Component,
+    Output,
+    EventEmitter
+} from '@angular/core';
 
 @Component({
     selector: 'note-creator',
@@ -18,7 +22,7 @@ import {Component} from '@angular/core';
     `],
     template: `
         <div class="note-creator shadow-2">
-          <form class="row">
+          <form class="row" (submit)="onCreateNote()">
             <input
               type="text"
               [(ngModel)]="newNote.title"
@@ -47,9 +51,29 @@ import {Component} from '@angular/core';
     `
 
 })
-export class NoteCreator{
+export class NoteCreator {
+
+    @Output() createNote = new EventEmitter();
+
     newNote = {
         title: '',
         value: ''
     };
+
+    onCreateNote() {
+        const {title, value} = this.newNote;
+        if (title && value) {
+            this.createNote.next({title, value});
+        }
+        this.reset()
+    }
+
+    reset() {
+        this.newNote = {
+            title: '',
+            value: ''
+        };
+    }
+
+
 }
