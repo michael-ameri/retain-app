48c8bfb25c1c2aed805bdb5d3dd0d7eeb13cca34
Michael <michael.ameri@gmail.com>
2016-12-03 12:44:21 +0100

    hello world

---
+++ src/app/app.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'app',
+    template: `
+    <div>
+        <h3>
+        hello world.
+        </h3>
+    </div>
+`
+
+})
+export class App {
+}
+;
